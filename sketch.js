7825baf80af949feee4bf77ee6f71445d6d696ff
Yuanqing <yl11249@nyu.edu>
2023-10-22 22:03:58 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,34 @@
+let ellipses = [];
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
+  background(220);
+
   if (mouseIsPressed) {
     fill(0);
   } else {
     fill(255);
   }
-  ellipse(mouseX, mouseY, 80, 80);
+
+  // Add a new ellipse at the mouse cursor's position
+  ellipses.push({ x: mouseX, y: mouseY, alpha: 255 });
+
+  // Limit the number of ellipses to 5
+  if (ellipses.length > 5) {
+    ellipses.shift();
+  }
+
+  // Draw and update the ellipses
+  for (let i = 0; i < ellipses.length; i++) {
+    let currentEllipse = ellipses[i];
+    ellipse(currentEllipse.x, currentEllipse.y, 80, 80);
+    currentEllipse.alpha -= 5; // Reduce the alpha (fade out)
+    fill(255, currentEllipse.alpha);
+  }
+
+  // Remove completely faded out ellipses
+  ellipses = ellipses.filter(ellipse => ellipse.alpha > 0);
 }
