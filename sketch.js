3b8e824e1bbfbac331bb82a467f29108dab9db37
Yuanqing <yl11249@nyu.edu>
2023-10-23 18:09:27 -0400

    moving lines

--- sketch.js
+++ sketch.js
@@ -1,34 +1,24 @@
-let ellipses = [];
+let numLines = 16;
+let startTime;
+let speed = 1; // Adjust the speed of movement
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth,windowHeight);
+  startTime = millis();
 }
 
 function draw() {
-  background(220);
-
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
+  let currentTime = millis() - startTime;
+  background(220, 10, 100);
+  for (let l = 0; l <= numLines; l += 1) {
+    let x0 = map(l, 0, numLines, 0, width);
+    let t = (currentTime * 0.001) * speed; // Adjust the speed factor
+    let x1 = map(l, 0, numLines, (4 / 10) * width, (6 / 10) * width);
+    x1 += map(t, 0, 1, 0, width - ((6 / 10) * width));
+    let x2 = map(l, 0, numLines, -width, 2 * width);
+    let y1 = map(t, 0, 1, height / 2, 0);
+    line(x0, 0, x1, y1);
+    line(x1, y1, x2, height);
   }
-
-  // Add a new ellipse at the mouse cursor's position
-  ellipses.push({ x: mouseX, y: mouseY, alpha: 255 });
-
-  // Limit the number of ellipses to 5
-  if (ellipses.length > 5) {
-    ellipses.shift();
-  }
-
-  // Draw and update the ellipses
-  for (let i = 0; i < ellipses.length; i++) {
-    let currentEllipse = ellipses[i];
-    ellipse(currentEllipse.x, currentEllipse.y, 80, 80);
-    currentEllipse.alpha -= 5; // Reduce the alpha (fade out)
-    fill(255, currentEllipse.alpha);
-  }
-
-  // Remove completely faded out ellipses
-  ellipses = ellipses.filter(ellipse => ellipse.alpha > 0);
 }
+
